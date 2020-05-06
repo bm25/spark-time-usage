8555de10e3a161243f782c252ee1112613e3e98b
anton <avokadoroll@gmail.com>
2020-05-07 01:42:57 +0300

    classifiedColumns() was made independent from DataFrame: column names are taken from separate csv-file

--- src/test/scala/timeusage/TimeUsageSuite.scala
+++ src/test/scala/timeusage/TimeUsageSuite.scala
@@ -3,17 +3,12 @@ package timeusage
 import org.apache.spark.sql.functions.expr
 import org.apache.spark.sql.{ColumnName, DataFrame, Row}
 import org.junit.{Assert, Test}
-import org.junit.Assert.assertEquals
-import timeusage.TimeUsage.{columns, initDf, read}
-
-import scala.util.Random
 
 class TimeUsageSuite {
 
   def initializeTimeUsage(): Boolean =
     try {
       TimeUsage
-
       true
     } catch {
       case ex: Throwable =>
@@ -24,9 +19,13 @@ class TimeUsageSuite {
 
   import TimeUsage._
 
+  //object DefaultDf {
+  //  val df: DataFrame = read(PATH_TO_DATA)._2
+  //}
+
   @Test def `'classifiedColumns' should return 3 lists of columns`: Unit = {
     assert(initializeTimeUsage(), " -- initialization failed")
-    val (primaryNeedsColumns, workColumns, otherColumns) = classifiedColumns(getColumns)
+    val (primaryNeedsColumns, workColumns, otherColumns) = classifiedColumns(COLUMN_NAMES)
     assert(primaryNeedsColumns.size == 55, "primaryNeedsColumns.size should equal to 55")
     assert(workColumns.size == 23, "workColumns should equal to 23")
     assert(otherColumns.size == 346, "otherColumns should equal to 346")
@@ -34,8 +33,8 @@ class TimeUsageSuite {
 
   @Test def `'timeUsageSummary' should return total time per row equal to 24 hours`: Unit = {
     assert(initializeTimeUsage(), " -- initialization failed")
-    val (primaryNeedsColumns, workColumns, otherColumns) = classifiedColumns(getColumns)
-    val df = timeUsageSummary(primaryNeedsColumns, workColumns, otherColumns, getInitDf)
+    val (primaryNeedsColumns, workColumns, otherColumns) = classifiedColumns(COLUMN_NAMES)
+    val df = timeUsageSummary(primaryNeedsColumns, workColumns, otherColumns, read(PATH_TO_DATA)._2)
     val dfOfIncorrectTotalTimePerDay = df
           .withColumn("sumTimeUsagePerRow", expr("primaryNeeds + work + other"))
           .where("(sumTimeUsagePerRow <= 0 or sumTimeUsagePerRow >= 24.1)")
@@ -47,8 +46,8 @@ class TimeUsageSuite {
 
   @Test def `'timeUsageGrouped' should return 12 rows of 6 columns`: Unit = {
     assert(initializeTimeUsage(), " -- initialization failed")
-    val (primaryNeedsColumns, workColumns, otherColumns) = classifiedColumns(getColumns)
-    var df = timeUsageSummary(primaryNeedsColumns, workColumns, otherColumns, getInitDf)
+    val (primaryNeedsColumns, workColumns, otherColumns) = classifiedColumns(COLUMN_NAMES)
+    var df = timeUsageSummary(primaryNeedsColumns, workColumns, otherColumns, read(PATH_TO_DATA)._2)
     df = timeUsageGrouped(df)
     assert(df.count() == 12, "there should be 12 rows in final dataset")
     assert(df.columns.size == 6, "there should be 12 rows in final dataset")
@@ -56,8 +55,8 @@ class TimeUsageSuite {
 
   @Test def `'timeUsageGroupedSql' should return 12 rows of 6 columns`: Unit = {
     assert(initializeTimeUsage(), " -- initialization failed")
-    val (primaryNeedsColumns, workColumns, otherColumns) = classifiedColumns(getColumns)
-    var df = timeUsageSummary(primaryNeedsColumns, workColumns, otherColumns, getInitDf)
+    val (primaryNeedsColumns, workColumns, otherColumns) = classifiedColumns(COLUMN_NAMES)
+    var df = timeUsageSummary(primaryNeedsColumns, workColumns, otherColumns, read(PATH_TO_DATA)._2)
     df = timeUsageGroupedSql(df)
     assert(df.count() == 12, "there should be 12 rows in final dataset")
     assert(df.columns.size == 6, "there should be 12 rows in final dataset")
@@ -65,8 +64,8 @@ class TimeUsageSuite {
 
   @Test def `'timeUsageGroupedTyped' should return 12 rows of 6 columns`: Unit = {
     assert(initializeTimeUsage(), " -- initialization failed")
-    val (primaryNeedsColumns, workColumns, otherColumns) = classifiedColumns(getColumns)
-    val df = timeUsageSummary(primaryNeedsColumns, workColumns, otherColumns, getInitDf)
+    val (primaryNeedsColumns, workColumns, otherColumns) = classifiedColumns(COLUMN_NAMES)
+    val df = timeUsageSummary(primaryNeedsColumns, workColumns, otherColumns, read(PATH_TO_DATA)._2)
     var ds = timeUsageSummaryTyped(df)
     ds = timeUsageGroupedTyped(ds)
     ds.show()
